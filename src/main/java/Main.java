19ab5c873ac004b2b74198764e23ca851f0be876
luo <luo@qq.com>
2018-11-14 20:28:44 +0800

    架构搭建完成

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author yifeng
+ * @Time 2018/11/14 20:19
+ * @Version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
